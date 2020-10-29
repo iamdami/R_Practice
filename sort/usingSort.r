ba98152b6342fad85cf48a8fdca127705ed1e624
iamdami <50016477+iamdami@users.noreply.github.com>
2020-10-29 19:50:42 +0900

    usingSort.r

---
+++ sort/usingSort.r
@@ -0,0 +1,13 @@
+v1 <- c(3, 7, 2, 6)
+v2 <- c(3, 7, 2, NA, 6)
+
+#sort()
+#Return the sorted data immediately (rearrange according to the order and output the value)
+
+sort(v1)
+
+sort(v1, decreasing = T)
+#Sort Descending
+ 
+sort(v2)
+#Ignore NA (default)
